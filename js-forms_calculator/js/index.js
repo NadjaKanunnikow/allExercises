ad5411de0ee1be7bfe895d49bb5f2dcf30051c83
Nadja Kanunnikow <nadja.kanunnikow@gmx.de>
2023-07-21 08:56:49 +0200

    add exercise calculate

---
+++ js-forms_calculator/js/index.js
@@ -0,0 +1,46 @@
+console.clear();
+
+const form = document.querySelector('[data-js="form"]');
+const resultOutput = document.querySelector('[data-js="result"]');
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let result;
+
+  // --v-- write your code here --v--
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+
+  const numberA = Number.parseInt(data.numberA, 10);
+  const numberB = Number.parseInt(data.numberB, 10);
+
+  if (data.operator === "addition") {
+    result = add(numberA, numberB);
+  } else if (data.operator === "subtraction") {
+    result = subtract(numberA, numberB);
+  } else if (data.operator === "multiplication") {
+    result = multiply(numberA, numberB);
+  } else if (data.operator === "divide") {
+    result = divide(numberA, numberB);
+  }
+  // --^-- write your code here --^--
+
+  resultOutput.textContent = result;
+});
