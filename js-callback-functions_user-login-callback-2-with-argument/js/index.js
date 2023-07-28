d3f6811eb24d283163145708806fc2c4d7323601
Nadja Kanunnikow <nadja.kanunnikow@gmx.de>
2023-07-28 11:43:01 +0200

    add exercise callback with parameters

---
+++ js-callback-functions_user-login-callback-2-with-argument/js/index.js
@@ -0,0 +1,16 @@
+console.clear();
+
+function handleUserLogin(onSuccess) {
+  onSuccess("Jane Doe");
+}
+
+// The exercise starts here!
+
+function showWelcomeMessage(userName) {
+  console.log("Welcome Jane Doe! You are logged in now.");
+}
+handleUserLogin(showWelcomeMessage);
+
+handleUserLogin(() => {
+  console.log("Welcome Jane Doe! You are logged in now.");
+});
