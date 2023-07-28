81481e8c5f0d51d62c06670ee6ca744a46d3d08d
Nadja Kanunnikow <nadja.kanunnikow@gmx.de>
2023-07-28 11:18:51 +0200

    add exercise callback named vs. anonymous

---
+++ js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
@@ -0,0 +1,19 @@
+console.clear();
+
+function handleUserLogin(onSuccess) {
+  // You might wonder why this function is called `onSuccess`. This will become clear by the fourth challenge! 🤞
+  onSuccess();
+}
+
+// Call handleUserLogin below!
+
+function showWelcomeMessage() {
+  console.log("Welcome! You are logged in now.");
+}
+// showWelcomeMessage();
+
+handleUserLogin(showWelcomeMessage);
+
+handleUserLogin(() => {
+  console.log("hello");
+});
