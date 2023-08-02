20fc33f36642c0d2a64ecc58f450ee0c88bd154c
Nadja Kanunnikow <nadja.kanunnikow@gmx.de>
2023-08-02 13:33:44 +0200

    fix path in index.js

--- js-structure_quiz-app/index.js
+++ js-structure_quiz-app/index.js
@@ -1,10 +1,4 @@
-//IMPORT UTILS
-import { cards } from "./utils/cards.js";
-
 // IMPORT COMPONENTS
-import Header from "./components/Header/Header.js";
-import Form from "./components/Form/Form.js";
-import CardList from "./components/CardList/CardList.js";
 import App from "./components/App/App.js";
 
 // The render function renders the app to the DOM
