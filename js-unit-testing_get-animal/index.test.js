e73c6dbb90f7bdb8d7b56c0bb5474d0a242a5e66
Nadja Kanunnikow <nadja.kanunnikow@gmx.de>
2023-08-10 11:23:39 +0200

    save new question task 2

--- js-unit-testing_get-animal/index.test.js
+++ js-unit-testing_get-animal/index.test.js
@@ -4,6 +4,11 @@ test("getAnimals() returns a string 'I totally love cats!' if called with the ar
   const result = getAnimal("cats");
   expect(result).toBe("I totally love cats!");
 });
+
+// // test("getAnimals() returns a string 'I like [animalName]!' if called with a string argument that is not cat.", () => {
+//   const result = getAnimal("dogs");
+//   expect(result).toBe("I like dogs!");
+// });
 test("getAnimals() returns a string 'I like [animalPluralName]!' if called with a string argument.", () => {
   const result = getAnimal("dogs");
   expect(result).toBe("I like dogs!");
