71288a5bed770d7af22e46cda1a79d3c5321962e
Nadja Kanunnikow <nadja.kanunnikow@gmx.de>
2023-08-16 11:50:53 +0200

    add exercise react-basic_hello-world

---
+++ react-basics_hello-world/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return <HelloWorld />;
+}
+
+function HelloWorld() {
+  return (
+    <div>
+      Say hello...
+      <h1>Hello world!</h1>
+    </div>
+  );
+}
